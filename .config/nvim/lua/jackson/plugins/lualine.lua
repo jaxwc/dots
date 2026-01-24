ba238f1ffb627fd1db68c3d56596e3b357f74949
Jackson <jacksonwc@proton.me>
2026-01-23 22:03:37 -0600

    updated tmux

--- .config/nvim/lua/jackson/plugins/lualine.lua
+++ .config/nvim/lua/jackson/plugins/lualine.lua
@@ -14,7 +14,7 @@ return {
         lualine_b = { "branch", "diff", "diagnostics" },
         lualine_c = {
           { "filename", path = 0 },
-        },       
+        },
         lualine_x = { "filetype" },
         lualine_y = { "progress" },
         lualine_z = { "location" },
